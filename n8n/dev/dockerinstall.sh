cdaf53402786d93903fbe4b7135fbf9b1ba3298a
st84 <58313821+st84@users.noreply.github.com>
2026-02-18 08:02:35 -0800

    Refactor Docker installation in dockerinstall.sh
    Updated Docker installation script to use the new keyring method for adding Docker repository.

--- n8n/dev/dockerinstall.sh
+++ n8n/dev/dockerinstall.sh
@@ -1,3 +1,10 @@
-apt -y install docker.io docker-compose-plugin
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" \
+  > /etc/apt/sources.list.d/docker.list
+
+apt update
+apt -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 systemctl enable --now docker
-usermod -aG docker admin
